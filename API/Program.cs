eee3f3d779a45f8d1c6e6a981f1d9ccf2ff35228
vinaysoft62 <vinaysoft62@gmail.com>
2025-01-10 17:44:02 +0530

    End of Section 2

---
+++ API/Program.cs
@@ -0,0 +1,36 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//DB service registered by Vinay(on 9th Jan,2025)
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+   opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+   // app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
